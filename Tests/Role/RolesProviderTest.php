01d879196713ffb38127b54a598bda163f585e65
Simon Libaud <simonlibaud@gmail.com>
2017-07-27 11:44:31 +0200

    [UnitTests] RolesProviderTest

---
+++ Tests/Role/RolesProviderTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Sil\RouteSecurityBundle\Tests\Role;
+
+use PHPUnit\Framework\TestCase;
+use Sil\RouteSecurityBundle\Role\RolesProvider;
+use Sil\RouteSecurityBundle\Role\RouteToRoleConverter;
+
+class RolesProviderTest extends TestCase
+{
+    public function testGetRoles()
+    {
+        $secured_routes = ['route_foo', 'route_bar'];
+        $routeToRoleConverter = $this->createMock(RouteToRoleConverter::class);
+        $routeToRoleConverter
+            ->expects(new \PHPUnit_Framework_MockObject_Matcher_InvokedCount(count($secured_routes)))
+            ->method('generateRoleForRoute')
+            ->will($this->returnCallback(function ($route) {
+                return 'ROLE_'.strtoupper($route);
+            }));
+        $rolesProvider = new RolesProvider($routeToRoleConverter, $secured_routes);
+        $this->assertEquals(['ROLE_ROUTE_FOO', 'ROLE_ROUTE_BAR'], $rolesProvider->getRoles());
+    }
+}
